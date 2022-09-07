0b97ca5bba610b16971dfca59428cc86a7474560
gurubagawan <guru.bagawan@gmail.com>
2022-09-06 20:50:40 -0400

    testing util export

--- cypress/integration/1-base.spec.js
+++ cypress/integration/1-base.spec.js
@@ -1,41 +1,45 @@
 const { stringify } = require('mocha/lib/utils')
 const X2JS = require('x2js')
-const { fullMap, checkMetaTag } = require('../support')
+const { fullMap, checkMetaTag, checkCrawler } = require('../support')
 const axios = require('axios').default
 
 
-fullMap.forEach((sitemap, index)=> {
-	describe(`Map Group ${index+1} `, ()=>{
-		before(()=>{
-			cy.wait(10000)
-			cy.log(fullMap.length)
-		})
-		sitemap.forEach((obj)=>{
-				for (let j = 0; j < 100; j+=10) {
-				for (let i = 0; i < 10; i++) {
-					it(`Tests URL ${obj.loc}`,()=>{
-						cy.request(obj.loc).then((resp)=>{
-							const x2js = new X2JS()
-							// console.log(resp)
-							const innerJSON = x2js.xml2js(resp.body)
-							if(innerJSON.urlset?.url[1]){
-								if(!innerJSON.urlset?.url[i+j]){return}
-								cy.visit(innerJSON.urlset.url[i+j].loc)
-								checkMetaTag()
-								cy.get('.td-sub-footer-menu').should('exist')
-							} else {
-								if(!innerJSON.urlset?.url || i > 1){
-									return
-								}
-								cy.visit(innerJSON.urlset.url.loc)
-								checkMetaTag()
-								cy.get('.td-sub-footer-menu').should('exist')
-							}
-						})
-					});
-				}
-			}
-			})
-		})
-	})
+// fullMap.forEach((sitemap, index)=> {
+// 	describe(`Map Group ${index+1} `, ()=>{
+// 		before(()=>{
+// 			cy.wait(10000)
+// 			cy.log(fullMap.length)
+// 		})
+// 		sitemap.forEach((obj)=>{
+// 				for (let j = 0; j < 100; j+=10) {
+// 				for (let i = 0; i < 10; i++) {
+// 					it(`Tests URL ${obj.loc}`,()=>{
+// 						cy.request(obj.loc).then((resp)=>{
+// 							const x2js = new X2JS()
+// 							// console.log(resp)
+// 							const innerJSON = x2js.xml2js(resp.body)
+// 							if(innerJSON.urlset?.url[1]){
+// 								if(!innerJSON.urlset?.url[i+j]){return}
+// 								cy.visit(innerJSON.urlset.url[i+j].loc)
+// 								checkMetaTag()
+// 								cy.get('.td-sub-footer-menu').should('exist')
+// 							} else {
+// 								if(!innerJSON.urlset?.url || i > 1){
+// 									return
+// 								}
+// 								cy.visit(innerJSON.urlset.url.loc)
+// 								checkMetaTag()
+// 								cy.get('.td-sub-footer-menu').should('exist')
+// 							}
+// 						})
+// 					});
+// 				}
+// 			}
+// 			})
+// 		})
+// 	})
+
+for (let j = 0; j < 100; j+=10) {
+	checkCrawler(j)
+}
 	
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -212,6 +212,41 @@ export const checkMetaTag = () =>{
     });
 }
 
+export const checkCrawler = (j) => { 
+	fullMap.forEach((sitemap, index)=> {
+		describe(`Map Group ${index+1} `, ()=>{
+			before(()=>{
+				cy.wait(10000)
+				cy.log(fullMap.length)
+			})
+			sitemap.forEach((obj)=>{
+				for (let i = 0; i < 10; i++) {
+					it(`Tests URL ${obj.loc}`,()=>{
+						cy.request(obj.loc).then((resp)=>{
+							const x2js = new X2JS()
+							// console.log(resp)
+							const innerJSON = x2js.xml2js(resp.body)
+							if(innerJSON.urlset?.url[1]){
+								if(!innerJSON.urlset?.url[i+j]){return}
+								cy.visit(innerJSON.urlset.url[i+j].loc)
+								checkMetaTag()
+								cy.get('.td-sub-footer-menu').should('exist')
+							} else {
+								if(!innerJSON.urlset?.url || i > 1){
+									return
+								}
+								cy.visit(innerJSON.urlset.url.loc)
+								checkMetaTag()
+								cy.get('.td-sub-footer-menu').should('exist')
+							}
+						})
+					});
+				}
+			})
+		})
+	})
+}
+
 export const fullMap = [
 	[
 		{
