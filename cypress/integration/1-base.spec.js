12df5debd9b22e304c6edd505f24b66be51f889c
gurubagawan <guru.bagawan@gmail.com>
2022-09-14 11:15:59 -0400

    changed to single loop

--- cypress/integration/1-base.spec.js
+++ cypress/integration/1-base.spec.js
@@ -39,46 +39,127 @@ const axios = require('axios').default
 // 		})
 // 	})
 
-for (let index = 0; index < 1000; index+= 100) {
 	
-	for (let j = 0; j < 10; j++) {
-		checkCrawler(j)
-	}
+for (let j = 0; j < 10; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 10; j < 20; j++) {
-		checkCrawler(j+index)
-	}
+for (let j = 10; j < 20; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 20; j < 30; j++) {
-		checkCrawler(j)
-	}
+for (let j = 20; j < 30; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 30; j < 40; j++) {
-		checkCrawler(j)
-	}
+for (let j = 30; j < 40; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 40; j < 50; j++) {
-		checkCrawler(j)
-	}
+for (let j = 40; j < 50; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 50; j < 60; j++) {
-		checkCrawler(j)
-	}
+for (let j = 50; j < 60; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 60; j < 70; j++) {
-		checkCrawler(j)
-	}
+for (let j = 60; j < 70; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 70; j < 80; j++) {
-		checkCrawler(j)
-	}
+for (let j = 70; j < 80; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 80; j < 90; j++) {
-		checkCrawler(j)
-	}
+for (let j = 80; j < 90; j++) {
+	checkCrawler(j)
+}
 
-	for (let j = 90; j < 100; j++) {
-		checkCrawler(j)
-	}
+for (let j = 90; j < 100; j++) {
+	checkCrawler(j)
+}
 
+
+
+for (let j = 100; j < 110; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 110; j < 120; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 120; j < 130; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 130; j < 140; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 140; j < 150; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 150; j < 160; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 160; j < 170; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 170; j < 180; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 180; j < 190; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 190; j < 200; j++) {
+	checkCrawler(j)
+}
+
+
+
+for (let j = 200; j < 210; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 210; j < 220; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 220; j < 230; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 230; j < 240; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 240; j < 250; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 250; j < 260; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 260; j < 270; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 270; j < 280; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 280; j < 290; j++) {
+	checkCrawler(j)
+}
+
+for (let j = 290; j < 300; j++) {
+	checkCrawler(j)
 }
