870ae77f5396ab35d7e71de0fea408e7837bc31c
gurubagawan <guru.bagawan@gmail.com>
2022-10-11 10:58:42 -0400

    fixed function

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -26,12 +26,12 @@ module.exports = (on, config) => {
   return config;
 };
 
-module.exports = (on, config) => {
-  on('task', {
-    log(message) {
-      console.log(message)
+// module.exports = (on, config) => {
+//   on('task', {
+//     log(message) {
+//       console.log(message)
   
-      return null
-    },
-  })
-}
+//       return null
+//     },
+//   })
+// }
